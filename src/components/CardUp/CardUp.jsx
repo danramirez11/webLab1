13ac4fb22703dabefca7c775622259dd4e929cd2
Dan <daramirezp1112@gmail.com>
2024-08-14 23:06:43 -0500

    feat responsive up cards

--- src/components/CardUp/CardUp.jsx
+++ src/components/CardUp/CardUp.jsx
@@ -7,8 +7,12 @@ function CardUp ({data, random}){
         <>
         <div style={{ 
             backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)),url(${data.image})`, 
-            width: random ? "35%" : "15%"}} className="card cardUp">
-            <div style={{ backgroundColor: data.tag === 'Product' ? "#4fd0d3" : "#5e9bff"}} className="tag">
+            width: random ? "35%" : "15%",
+            
+            }} className="card cardUp">
+            <div style={{ 
+                backgroundColor: data.tag === 'Product' ? "#4fd0d3" : "#5e9bff"}} 
+                className='tag'>
                 <p>{data.tag}</p>
             </div>
             <p>{data.desc}</p>
